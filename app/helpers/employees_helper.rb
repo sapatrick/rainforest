8ef7ceb0789f3beb7c20fa96ba6d112e6506411d
Samantha <sa.patrick@yahoo.com>
2018-11-26 07:01:18 -0600

    Created an Employee Helper method to properly display first and last name together to eliminate duplication/extra code. Changed the employees index file to reflect that so the list is by employee. Still need to sort ascending or descending though.

--- app/helpers/employees_helper.rb
+++ app/helpers/employees_helper.rb
@@ -1,2 +1,9 @@
 module EmployeesHelper
+  def last_name_first(employee)
+  	"#{employee.last_name}, #{employee.first_name}"
+  end
+
+  def first_name_first(employee)
+  	"#{employee.first_name}, #{employee.last_name}"
+  end
 end
