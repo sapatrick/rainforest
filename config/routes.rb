418a9a6396dddcbe554b625e9c16213add3937b9
Samantha <sa.patrick@yahoo.com>
2018-11-26 06:47:42 -0600

    Added an index controller and a view. Set index as root path. Added a header file to layout view.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :employees
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+	root "index#index"
 end
